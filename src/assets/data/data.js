e8353a34756af04c61aa13a3dfd49d0d325a0d46
Taupik Rusmadi <255695782+taurus24fotocopy@users.noreply.github.com>
2026-03-16 11:30:22 +0700

    Upload file

---
+++ src/assets/data/data.js
@@ -0,0 +1,128 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Muhamad Rifaldi, S.Kom',
+            child: 'Putra',
+            father: 'Ms. Nano Prayogo',
+            mother: 'N. Denih',
+            salamat: 'Kedung Menjangan - Cirebon',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Irma Yunisa Isnaini',
+            child: 'Putri',
+            father: 'Iman Setiarasa, S.Tr.P',
+            mother: 'Siti Yuningsih',
+            salamat: 'Gunungmanik - Ciniru',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2026',
+            month: 'Februari',
+            date: '02',
+            day: 'Senin',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2026',
+            month: 'Februari',
+            date: '02',
+            day: 'Senin',
+            hours: {
+                start: '10.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Dusun Jatirasa RT.003 RW.002 Desa Gunungmanik Kec. Ciniru Kab. Kuningan - Jawa Barat'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/tmeEbH98eAiaiTVZ7',
+        map: 'https://maps.app.goo.gl/obKR4jmDTXcHkKdA6',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Irma Yunisa Isnaini',
+            icon: './src/assets/images/mandiri.png',
+            rekening: '1340029699559'
+        },
+        {
+            id: 2,
+            name: 'Muhamad Rifaldi',
+            icon: './src/assets/images/bni.png',
+            rekening: '1794083532'
+        },
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbwpQSZB2T7Sh3CqRdUILDYwO7AKEfQx2Smz8WWqV1j1kMZsl714ZOYdbU29JLOsnR3u7Q/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
---
+++ src/js/bride.js
@@ -0,0 +1,20 @@
+import {data} from "../assets/data/data.js";
+import {renderElement} from "../utils/helper.js";
+
+export const bride = () => {
+    const brideCouple = document.querySelector('.bride_couple ul');
+    const brideListItem = (data) => (
+        `<li data-aos="zoom-in" data-aos-duration="1000">
+              <figure>
+                   <img src=${data.image} alt="${data.name} animation">
+                   <figcaption>${data.name}</figcaption>
+              </figure>
+              <p>${data.child} <br>dari <br> Bapak ${data.father} & Ibu ${data.mother} <br>( ${data.salamat} )<br></p>
+              <span style="display: ${data.id === 1 ? 'none' : 'block'}">&</span>
+        </li>`
+    )
+
+    const brideData = [data.bride.P, data.bride.L];
+
+    renderElement(brideData, brideCouple, brideListItem);
+}
